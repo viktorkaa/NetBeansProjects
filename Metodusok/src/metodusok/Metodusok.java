0c29a18be3eddc23095977f2eddb3f02e058bb21
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-10-18 11:31:32 +0200

    Sztring Metodusok
    Sztring Metodusok

--- Metodusok/src/metodusok/Metodusok.java
+++ Metodusok/src/metodusok/Metodusok.java
@@ -46,6 +46,39 @@ public class Metodusok {
         
         //végződik e
         System.out.println(szoveg.endsWith(" "));
+        
+        //kezdődik valamivel
+        System.out.println(szoveg.startsWith(" "));
+        
+        //keresett karakter(ek) pozicioja
+        System.out.println(szoveg.indexOf("t"));
+        
+        //keresett karakter(ek) első pozicioja
+        System.out.println(szoveg.indexOf("t"));
+        System.out.println(szoveg.indexOf("egy"));
+        
+        //keresett karakter(ek) utolso pozicioja
+        System.out.println(szoveg.indexOf("t"));
+        
+        //string üres-e (boolean)
+        System.out.println(szoveg.isEmpty());
+        String szoveg2 = "";
+        System.out.println(szoveg2.isEmpty());
+       
+        //lecseréli a szövegben a megadott karakter(ek) et más karakterre
+        System.out.println(szoveg.replace("tetszőleges","akármilyen"));
+        System.out.println(szoveg.replace("e","b"));
+        
+        //string szétszedése tömbbe megadott karakterek mentén
+        System.out.println(szoveg.split(" "));
+        
+        //nem string típusú adat stringgé alakítása
+        //Ineteger szam = 512;
+        //System.out.println(szoveg.split(" "));
+        
+        //string szétszedése tömbbe megadott karakterek mentén
+        System.out.println(szoveg.substring(3));
+        System.out.println(szoveg.substring(3,6));
     }
     
 }
