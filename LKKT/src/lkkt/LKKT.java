4804ad91e47af124440bc16f5c2cf4a30758b527
viktorkaa <83224403+viktorkaa@users.noreply.github.com>
2022-10-18 09:54:57 +0200

    10.18
    10.18

--- LKKT/src/lkkt/LKKT.java
+++ LKKT/src/lkkt/LKKT.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package lkkt;
-
+import java.util.Scanner;
 /**
  *
  * @author user3
@@ -16,6 +16,28 @@ public class LKKT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+    int a, b, max, step, lcm = 0;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Adja meg az első számot :");
+      a = sc.nextInt();
+      System.out.println("Adja meg a második számot :");
+      b = sc.nextInt();
+
+      if(a > b){
+         max = step = a;
+      }
+      else{
+         max = step = b;
+      }
+
+      while(a!= 0) {
+         if(max % a == 0 && max % b == 0) {
+            lcm = max;
+            break;
+         }
+         max += step;
+      }
+      System.out.println("A legkissebb közös többszörösük:: "+lcm);
+   }
 }
+
